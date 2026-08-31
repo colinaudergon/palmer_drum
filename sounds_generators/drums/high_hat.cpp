b94b873998b8dd59bb312413dc2a1ab37a543608
colinaudergon <63097004+colinaudergon@users.noreply.github.com>
2026-08-31 20:23:08 +0200

    Updated high hat parameters usage

--- sounds_generators/drums/high_hat.cpp
+++ sounds_generators/drums/high_hat.cpp
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
@@ -28,6 +28,7 @@
 
 #include "high_hat.h"
 
+#include <algorithm>
 #include <cstdio>
 
 #include "../../lib/mu_dsp.h"
@@ -35,39 +36,77 @@
 #include "../peaks_ressources/resources.h"
 #include "../peaks_ressources/random.h"
 
-namespace peaks {
+namespace peaks
+{
 
-using namespace mu_stmlib;
+  using namespace mu_stmlib;
 
-void HighHat::Init() {
-  noise_.Init();
-  noise_.set_frequency(105 << 7);  // 8kHz
-  noise_.set_resonance(24000);
-  
-  vca_coloration_.Init();
-  vca_coloration_.set_frequency(110 << 7);  // 13kHz
-  vca_coloration_.set_resonance(0);
-  
-  vca_envelope_.Init();
-  vca_envelope_.set_delay(0);
-  vca_envelope_.set_decay(4093);
-}
+  void HighHat::Init()
+  {
+    noise_.Init();
+    noise_.set_frequency(105 << 7); // 8kHz
+    noise_.set_resonance(24000);
 
-void HighHat::Process(const GateFlags* gate_flags, int16_t* out, size_t size) {
-  while (size--) {
-    GateFlags gate_flag = *gate_flags++;
-    if (gate_flag & GATE_FLAG_RISING) {
-      vca_envelope_.Trigger(32768 * 15);
+    vca_coloration_.Init();
+    vca_coloration_.set_frequency(110 << 7); // 13kHz
+    vca_coloration_.set_resonance(0);
+
+    vca_envelope_.Init();
+    vca_envelope_.set_delay(0);
+    vca_envelope_.set_decay(kDefaultDecay);
+    envelope_value_ = 0;
+    envelope_update_period_ = 1;
+    envelope_update_counter_ = 0;
+  }
+
+  void HighHat::Process(const GateFlags *gate_flags, int16_t *out, size_t size)
+  {
+    while (size--)
+    {
+      GateFlags gate_flag = *gate_flags++;
+      if (gate_flag & GATE_FLAG_RISING)
+      {
+        vca_envelope_.Trigger(32768 * 15);
+        envelope_update_counter_ = 0;
+      }
+
+      HandleOsc();
+
+      int32_t filtered_noise = HandleNoise();
+
+      if (envelope_update_counter_ == 0)
+      {
+        envelope_value_ = vca_envelope_.Process() >> 4;
+        envelope_update_counter_ = envelope_update_period_ - 1;
+      }
+      else
+      {
+        --envelope_update_counter_;
+      }
+
+      int32_t vca_noise = envelope_value_ * filtered_noise >> 14;
+      CLIP(vca_noise);
+      int32_t hh = 0;
+      hh += vca_coloration_.Process<SVF_MODE_HP>(vca_noise);
+      hh += vca_coloration_.Process<SVF_MODE_HP>(vca_noise);
+      hh <<= 1;
+      CLIP(hh);
+      *out++ = hh;
     }
-  
-    phase_[0] += 48318382;
-    phase_[1] += 71582788;
-    phase_[2] += 37044092;
-    phase_[3] += 54313440;
-    phase_[4] += 66214079;
-    phase_[5] += 93952409;
+  }
 
+  void HighHat::Configure(uint16_t *parameter)
+  {
+    envelope_update_period_ = Map(parameter[0], 0, 65535, 1, kMaxEnvelopeUpdatePeriod);
+    tone_repeat_processing_ = Map(parameter[1], 0, 65535, 1, 16);
+    noise_.set_resonance(parameter[2]);
+    noise_mixer_ = parameter[3];
+  }
+
+  int32_t HighHat::HandleNoise()
+  {
     int16_t noise = 0;
+    // 
     noise += phase_[0] >> 31;
     noise += phase_[1] >> 31;
     noise += phase_[2] >> 31;
@@ -75,29 +114,40 @@ void HighHat::Process(const GateFlags* gate_flags, int16_t* out, size_t size) {
     noise += phase_[4] >> 31;
     noise += phase_[5] >> 31;
     noise <<= 12;
-  
     // Run the SVF at the double of the original sample rate for stability.
     int32_t filtered_noise = 0;
-    filtered_noise += noise_.Process<SVF_MODE_BP>(noise);
-    filtered_noise += noise_.Process<SVF_MODE_BP>(noise);
-
-    // The 808-style VCA amplifies only the positive section of the signal.
-    if (filtered_noise < 0) {
-      filtered_noise = 0;
-    } else if (filtered_noise > 32767) {
-      filtered_noise = 32767;
+    
+    for(size_t i = 0; i <tone_repeat_processing_; i++)
+    {
+      filtered_noise += noise_.Process<SVF_MODE_BP>(noise);
     }
-  
-    int32_t envelope = vca_envelope_.Process() >> 4;
-    int32_t vca_noise = envelope * filtered_noise >> 14;
-    CLIP(vca_noise);
-    int32_t hh = 0;
-    hh += vca_coloration_.Process<SVF_MODE_HP>(vca_noise);
-    hh += vca_coloration_.Process<SVF_MODE_HP>(vca_noise);
-    hh <<= 1;
-    CLIP(hh);
-    *out++ = hh;
+    
+    constexpr int32_t kMinimumNoise = -32768;
+    constexpr int32_t kMaximumNoise = 32767;
+    int32_t unrectified_noise = std::clamp(filtered_noise, kMinimumNoise, kMaximumNoise);
+    int32_t half_wave_noise = std::max(unrectified_noise, int32_t{0});
+    int64_t difference = unrectified_noise - half_wave_noise;
+    return half_wave_noise +
+           static_cast<int32_t>((difference * noise_mixer_ + 32767) / 65535);
   }
-}
 
-}  // namespace peaks
+  void HighHat::HandleOsc()
+  {
+
+    phase_[0] += 48318382;
+    phase_[1] += 71582788;
+    phase_[2] += 37044092;
+    phase_[3] += 54313440;
+    phase_[4] += 66214079;
+    phase_[5] += 93952409;
+  }
+
+  uint16_t HighHat::Map(uint16_t x, uint16_t in_min, uint16_t in_max, uint16_t out_min, uint16_t out_max)
+  {
+    uint32_t input_range = in_max - in_min;
+    uint32_t output_range = out_max - out_min;
+    uint32_t scaled = static_cast<uint32_t>(x - in_min) * output_range;
+    return static_cast<uint16_t>(scaled / input_range + out_min);
+  }
+
+} // namespace peaks
--- sounds_generators/drums/high_hat.h
+++ sounds_generators/drums/high_hat.h
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,13 +19,13 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
 //
 // 808-style HH.
- 
+
 #ifndef PEAKS_DRUMS_HIGH_HAT_H_
 #define PEAKS_DRUMS_HIGH_HAT_H_
 
@@ -36,27 +36,48 @@
 
 #include "../peaks_ressources/gate_processor.h"
 
-namespace peaks {
+namespace peaks
+{
 
-class HighHat {
- public:
-  HighHat() { }
-  ~HighHat() { }
+  class HighHat
+  {
+  public:
+    HighHat() {}
+    ~HighHat() {}
 
-  void Init();
-  void Process(const GateFlags* gate_flags, int16_t* out, size_t size);
-  void Configure(uint16_t* parameter) { }
-  
- private:
-  Svf noise_;
-  Svf vca_coloration_;
-  Excitation vca_envelope_;
-  
-  uint32_t phase_[6];
+    void Init();
+    void Process(const GateFlags *gate_flags, int16_t *out, size_t size);
+    void Configure(uint16_t *parameter);
 
-  DISALLOW_COPY_AND_ASSIGN(HighHat);
-};
+  private:
+    Svf noise_;
+    Svf vca_coloration_;
+    Excitation vca_envelope_;
+    int32_t envelope_value_ = 0;
+    uint16_t envelope_update_period_ = 1;
+    uint16_t envelope_update_counter_ = 0;
+    size_t tone_repeat_processing_ = 1;
+    uint16_t noise_mixer_ = 0;
+    int32_t HandleNoise();
+    void HandleOsc();
+    uint16_t Map(uint16_t x, uint16_t in_min, uint16_t in_max, uint16_t out_min, uint16_t out_max);
+    uint32_t phase_[6];
+    static constexpr uint16_t kDefaultDecay = 4093;
+    static constexpr uint16_t kMaxEnvelopeUpdatePeriod = 16;
+    static constexpr size_t kNumberOfOsc = 6;
+    static constexpr int kDefaultFilterIncrement[] = {48318382,
+                                                      71582788,
+                                                      37044092,
+                                                      54313440,
+                                                      66214079,
+                                                      93952409};
 
-}  // namespace peaks
+    static constexpr uint16_t kDefaultResonance = 24000;
+    // static constexpr uint16_t kDefaultResonance = 24000;
 
-#endif  // PEAKS_DRUMS_HIGH_HAT_H_
+    DISALLOW_COPY_AND_ASSIGN(HighHat);
+  };
+
+} // namespace peaks
+
+#endif // PEAKS_DRUMS_HIGH_HAT_H_
